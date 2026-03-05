3ecdcae049c11246f95a49eb6073f216d386694d
nikdem1 <demid.nikita2016@yandex.ru>
2026-03-05 14:51:37 +0300

    add large binaries with LFS

--- dist/matplotlib/mpl-data/kpsewhich.lua
+++ dist/matplotlib/mpl-data/kpsewhich.lua
@@ -1,3 +1,3 @@
--- see dviread._LuatexKpsewhich
-kpse.set_program_name("latex")
-while true do print(kpse.lookup(io.read():gsub("\r", ""))); io.flush(); end
+version https://git-lfs.github.com/spec/v1
+oid sha256:45dc986819c12f2dcdb01e5cd91e451ab2aef95f9605c6629b6e0d5a296c4dfc
+size 139
